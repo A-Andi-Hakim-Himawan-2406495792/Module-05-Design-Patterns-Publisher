813d380d3810ebfda0eb37df6b33015dc5446c9b
unknown <hakimandi976@gmail.com>
2026-03-19 12:07:19 +0700

    Implement notify function in Notification service to notify each Subscriber.

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -1,15 +1,27 @@
 use crate::model::subscriber::Subscriber;
+use crate::model::notification::Notification;
 use crate::repository::subscriber::SubscriberRepository;
+use std::thread;
 
 pub struct NotificationService;
 
 impl NotificationService {
-        pub fn subscribe(product_type: String, subscriber: Subscriber) -> Subscriber {
-            SubscriberRepository::add(&product_type, subscriber)
+    pub fn subscribe(product_type: String, subscriber: Subscriber) -> Subscriber {
+        SubscriberRepository::add(&product_type, subscriber)
+    }
+
+    pub fn unsubscribe(product_type: String, subscriber_url: String) -> Option<Subscriber> {
+        SubscriberRepository::delete(&product_type, &subscriber_url)
+    }
+
+    pub fn notify(product_type: String, notification: Notification) {
+        let subscribers = SubscriberRepository::list_all(&product_type);
+
+        for subscriber in subscribers {
+            let notif_clone = notification.clone();
+            thread::spawn(move || {
+                subscriber.update(&notif_clone);
+            });
         }
-        pub fn unsubscribe(product_type: String, subscriber_url: String) -> Option<Subscriber> {
-                SubscriberRepository::delete(&product_type, &subscriber_url)
-            }
-
-
+    }
 }
