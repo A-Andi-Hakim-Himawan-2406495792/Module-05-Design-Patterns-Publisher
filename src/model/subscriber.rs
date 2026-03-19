7b42e7bd01fc3b682a6b73445e58df7a548d1fb2
unknown <hakimandi976@gmail.com>
2026-03-19 12:06:48 +0700

    Implement update method in Subscriber model to send notification HTTP requests.

--- src/model/subscriber.rs
+++ src/model/subscriber.rs
@@ -1,8 +1,19 @@
 use rocket::serde::{Deserialize, Serialize};
+use crate::model::notification::Notification;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Subscriber {
     pub id: String,
     pub name: String,
     pub url: String,
+}
+
+impl Subscriber {
+    pub fn update(&self, notification: &Notification) {
+        let client = reqwest::blocking::Client::new();
+        let _ = client
+            .post(&self.url)
+            .json(notification)
+            .send();
+    }
 }
