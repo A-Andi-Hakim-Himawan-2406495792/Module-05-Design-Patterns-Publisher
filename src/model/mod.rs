656ad0d3b415763e0e5dff7aa2a972a772cc24b0
unknown <hakimandi976@gmail.com>
2026-03-19 11:30:13 +0700

    Create Notification model struct.

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,1 +1,3 @@
 pub mod subscriber;
+pub mod product;
+pub mod notification;
---
+++ src/model/notification.rs
@@ -0,0 +1,11 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Notification {
+    pub product_type: String,
+    pub product_title: String,
+    pub subscriber_name: String,
+    pub subscriber_url: String,
+    pub status: String,
+    pub message: String,
+}
